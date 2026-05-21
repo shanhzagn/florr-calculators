87877f66a636f4159d6f431bafb42eb679e44c8b
shanhzagn <shawn.s.zhang.2011@gmail.com>
2026-05-22 08:59:42 +1000

    bug fixes

--- FlorrioCraftingCalculator.py
+++ FlorrioCraftingCalculator.py
@@ -2,31 +2,23 @@
 DesiredRarity = input("insert the rarity you are trying to craft into e.g mythic ultra super")
 PetalsAvailable = int(input("insert the number of petals you have to attempt"))
 if DesiredRarity == "unusual":
-    FailChance = int(0.36)
-else:
-    if DesiredRarity == "rare":
-        FailChance = int(0.68)
-    else:
-        if DesiredRarity == "epic":
-            FailChance = int(0.84)
-        else:
-            if DesiredRarity == "legendary":
-                FailChance = int(0.92)
-            else:
-                if DesiredRarity == "mythic":
-                    FailChance = int(0.96)
-                else:
-                    if DesiredRarity == "ultra":
-                        FailChance = int(0.98)
-                    else:
-                        if DesiredRarity == "super":
-                            FailChance = int(0.99)
-                        else:
-                            if DesiredRarity == "eternal":
-                                FailChance = int(0.999)
-                            else:
-                                print("invalid input")
-SuccessChance = int(1-FailChance)
-PetalsLostPerAtt = int(2.5*FailChance+5*FailChance)
-AverageAttempts = int(PetalsAvailable/PetalsLostPerAtt)
+    FailChance = 0.36
+elif DesiredRarity == "rare":
+    FailChance = 0.68
+elif DesiredRarity == "epic":
+    FailChance = 0.84
+elif DesiredRarity == "legendary":
+    FailChance = 0.92
+elif DesiredRarity == "mythic":
+    FailChance = 0.96
+elif DesiredRarity == "ultra":
+    FailChance = 0.98
+elif DesiredRarity == "super":
+    FailChance = 0.99
+elif DesiredRarity == "eternal":
+    FailChance = 0.999
+else: print("invalid input")
+SuccessChance = 1-FailChance
+PetalsLostPerAtt = 2.5*FailChance+5*SuccessChance
+AverageAttempts = PetalsAvailable/PetalsLostPerAtt
 print(AverageAttempts)
