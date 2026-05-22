871a05ca75a56b112a66362a1e58284ed5c9bc61
shanhzagn <shawn.s.zhang.2011@gmail.com>
2026-05-22 22:01:41 +1000

    Added calculation for the chance for one succes

--- FlorrioCraftingCalculator.py
+++ FlorrioCraftingCalculator.py
@@ -1,4 +1,4 @@
-# this program gives you the chance of crafting at least 1 petal of the desired rarity
+# this program gives you the chance of crafting at least 1 petal of the desired rarity and the average amount you craft
 DesiredRarity = input("insert the rarity you are trying to craft into e.g mythic ultra super")
 PetalsAvailable = int(input("insert the number of petals you have to attempt"))
 if DesiredRarity == "unusual":
@@ -19,6 +19,10 @@ elif DesiredRarity == "eternal":
     FailChance = 0.999
 else: print("invalid input")
 SuccessChance = 1-FailChance
+MaxSuccesses = int(PetalsAvailable/5)
 PetalsLostPerAtt = 2.5*FailChance+5*SuccessChance
-AverageAttempts = PetalsAvailable/PetalsLostPerAtt
+AverageAttempts = int(PetalsAvailable/PetalsLostPerAtt)
+ChanceForOne = 1-FailChance**AverageAttempts
+AverageSuccesses = int(AverageAttempts)
 print(AverageAttempts)
+print(ChanceForOne)
