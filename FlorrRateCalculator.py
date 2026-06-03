12a46a36ceab4b8a97f9aa28efe00d750e22fabb
shanhzagn <shawn.s.zhang.2011@gmail.com>
2026-06-03 19:45:57 +1000

    input sanitisation

--- FlorrRateCalculator.py
+++ FlorrRateCalculator.py
@@ -1,18 +1,26 @@
-FarmedRarity = input("Do you farm ultra mobs or mythic mobs? (ultra/mythic)")
 while True:
+    FarmedRarity = input("Do you farm ultra mobs or mythic mobs? (ultra/mythic)").strip().lower()
     if FarmedRarity == "mythic":
+        Urate = 0
         Mrate = float(input("Insert rate of mythic drop of desired petal as a percentage from the farmed mob e.g. glass from mythic soldier ant would be 0.9:"))/100
         Lrate = float(input("Insert rate of legendary drop of desired petal as a percentage from the farmed mob:"))/100
     elif FarmedRarity == "ultra":
         Urate = float(input("Insert rate of ultra drop of desired petal as a percentage from the farmed mob e.g. glass from ultra soldier ant would be 0.5:"))/100
         Mrate = float(input("Insert rate of mythic drop of desired petal as a percentage from the farmed mob:"))/100
         Lrate = float(input("Insert rate of legendary drop of desired petal as a percentage from the farmed mob:")) / 100
-    elif FarmedRarity not in ["mythic", "ultra"]
-        print("invalid")
+    elif FarmedRarity not in ["mythic", "ultra"]:
+        print("invalid, try again!")
+        continue
+    break
+while True:
     UltrasNeeded = int(input("How many ultras do you want?"))
-    Mrate = Mrate+Lrate/65
-    Urate = Urate+Mrate/128
-    RequiredKills = int(UltrasNeeded/Urate)
-    print(f"On average, you need to kill {RequiredKills} {FarmedRarity}s to get {UltrasNeeded} ultra petals.")
-else:
-    print("invalid input")
+    try:
+        UltrasNeeded = int(UltrasNeeded)
+    except Exception:
+        print("Not a number")
+        continue
+    break
+Mrate = Mrate+Lrate/65
+Urate = Urate+Mrate/128
+RequiredKills = int(UltrasNeeded/Urate)
+print(f"On average, you need to kill {RequiredKills} {FarmedRarity}s to get {UltrasNeeded} ultra petals.")
